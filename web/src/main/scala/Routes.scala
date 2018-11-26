4d6160aa0841c39bf315d1e9573a2d53611f58fc
Huebner <huebnerc@wwt.com>
2018-11-26 15:15:48 -0600

    use the greeting actors in the api

--- web/src/main/scala/Routes.scala
+++ web/src/main/scala/Routes.scala
@@ -1,17 +1,39 @@
 package com.cory.web
 
+import akka.actor.ActorRef
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.Directives._
-import com.cory.core.Printer.Greeting
+import akka.http.scaladsl.server.Route
+import akka.pattern.AskSupport
+import akka.util.Timeout
+import com.cory.core.Greeting
+import com.cory.core.GreetingTranslator.GreetingRequest
+
+import scala.concurrent.duration._
+import scala.util.Random
 
 trait JsonSupport extends SprayJsonSupport {
   import spray.json.DefaultJsonProtocol._
   implicit val greetingFormat = jsonFormat1(Greeting)
 }
 
-trait Routes extends JsonSupport {
-  lazy val routes: Route = path("greetings" / Remaining) {
-    name => get { complete { Greeting(name) } }
+trait Routes extends JsonSupport with AskSupport {
+  implicit val timeout: Timeout = 3.seconds
+  def routes(greetingTranslator: ActorRef): Route = pathPrefix("greetings") {
+    path("random" / Remaining) {
+      name => get {
+        val dialect = Random.shuffle(List("basic", "cowboy", "butler")).head
+        onSuccess(greetingTranslator ? GreetingRequest(dialect, name)) {
+          case greeting: Greeting => complete(greeting)
+        }
+      }
+    } ~
+    path("""\w+""".r / Remaining) {
+      (dialect, name) => get {
+        onSuccess(greetingTranslator ? GreetingRequest(dialect, name)) {
+          case greeting: Greeting => complete(greeting)
+        }
+      }
+    }
   }
 }
--- web/src/main/scala/Server.scala
+++ web/src/main/scala/Server.scala
@@ -1,8 +1,9 @@
 package com.cory.web
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import com.cory.core.{Greeter, GreetingTranslator}
 
 import scala.io.StdIn
 
@@ -11,8 +12,19 @@ object Server extends App with Routes {
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
 
+  def createGreeter(message: String): ActorRef = {
+    system.actorOf(Greeter.props(message), s"${message.replaceAll("\\s+", "")}Greeter")
+  }
+
+  val dialectMap = Map(
+    "cowboy" -> createGreeter("Howdy"),
+    "basic" -> createGreeter("Hello"),
+    "butler" -> createGreeter("Good day")
+  )
+  val greetingTranslator = system.actorOf(GreetingTranslator.props(dialectMap))
+
   val (url, port) = ("localhost", 1307)
-  val bindingFuture = Http().bindAndHandle(routes, url, port)
+  val bindingFuture = Http().bindAndHandle(routes(greetingTranslator), url, port)
 
   println(s"Running the server @ http://$url:$port/\nPress RETURN to stop...")
   StdIn.readLine()
