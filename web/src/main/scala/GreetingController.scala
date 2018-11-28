ac36c82bac677382c5e6939d71b85d5fecbb9df9
Cesilie Huebner <CesilieHuebner@Cesilies-MacBook-Pro.local>
2018-11-27 19:07:32 -0600

    removed double-path-prefix

--- web/src/main/scala/GreetingController.scala
+++ web/src/main/scala/GreetingController.scala
@@ -7,17 +7,17 @@ import com.cory.core.Greeting
 
 class GreetingController(greetingTranslator: ActorRef) extends Controller {
   private def toGreetingResponse(greetingMagnet: GreetingRequestMagnet): Route = {
-    complete { (greetingTranslator ? greetingMagnet.apply).mapTo[Greeting] }
+    complete {
+      (greetingTranslator ? greetingMagnet.apply).mapTo[Greeting]
+    }
   }
 
-  lazy val routes: Route = pathPrefix("greetings") {
-    pathPrefix("dialects") {
-      path("random" / Segment) {
-        name => get(toGreetingResponse(name))
-      } ~
-      path(Segment / Segment).as(ValidatedGreetingRequest) {
-        validatedRequest => get(toGreetingResponse(validatedRequest))
-      }
+  lazy val routes: Route = pathPrefix("greetings" / "dialects") {
+    path("random" / Segment) {
+      name => get(toGreetingResponse(name))
+    } ~
+    path(Segment / Segment).as(ValidatedGreetingRequest) {
+      validatedRequest => get(toGreetingResponse(validatedRequest))
     }
   }
 }
