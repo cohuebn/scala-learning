cd19bfd917f75860a45481b57f5b25696e01e5a2
Huebner <huebnerc@wwt.com>
2018-11-30 14:35:18 -0600

    dont manually serialize all the things

--- web/src/main/scala/JsonSupport.scala
+++
@@ -1,9 +0,0 @@
-package com.cory.web
-
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import com.cory.core.Greeting
-
-trait JsonSupport extends SprayJsonSupport {
-  import spray.json.DefaultJsonProtocol._
-  implicit val greetingFormat = jsonFormat1(Greeting)
-}
